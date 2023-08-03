2c9bfa000489179e51474f3d1271d86adf125899
Robaa <njuguiinirobert@gmail.com>
2023-08-03 16:22:23 +0300

    setting up swagger docs

--- src/main/java/com/brandyodhiambo/ThoughtHub/ThoughtHubApplication.java
+++ src/main/java/com/brandyodhiambo/ThoughtHub/ThoughtHubApplication.java
@@ -1,9 +1,12 @@
 package com.brandyodhiambo.ThoughtHub;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.util.Collections;
+import java.util.Date;
 
 @SpringBootApplication
 public class ThoughtHubApplication {
@@ -13,6 +16,13 @@ public class ThoughtHubApplication {
 		SpringApplication app = new SpringApplication(ThoughtHubApplication.class);
 		app.setDefaultProperties(Collections.singletonMap("server.port", "8089"));
 		app.run(args);
+	}
+
+	@Bean
+	CommandLineRunner runner () {
+		return args -> {
+			System.out.println("THOUGHT HUB APPLICATION STARTED SUCCESSFULLY AT PORT: 8089 ON " + new Date());
+		};
 	}
 
 }
